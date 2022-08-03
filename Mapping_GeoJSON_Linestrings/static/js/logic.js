c0a1629a0b0cefd6441ea8c400cff7b7ddacce89
mik-moo <heatherharrahlea@gmail.com>
2022-08-03 18:32:44 -0500

    adding updated linestrings

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -7,14 +7,14 @@ console.log("working");
 
 
 // We create the tile layer that will be the background of our map.
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-day-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -22,7 +22,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: light,
+  Light: light,
   Dark: dark
 };
 
@@ -30,7 +30,7 @@ let baseMaps = {
 let map = L.map('mapid', {
   center: [44.0, -80.0],
   zoom: 2,
-  layers: [light]
+  layers: [dark]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -46,18 +46,25 @@ let torontoData = "https://raw.githubusercontent.com/mik-moo/Mapping_Earthquakes
 // // Accessing the airport GeoJSON URL
 // let airportData = "https://raw.githubusercontent.com/mik-moo/Mapping_Earthquakes/main/majorAirports.json";
 
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
+
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data
 //L.geoJson(data).addTo(map); 
 // Creating a GeoJSON layer with the retrieved data (binding to loop airport code and name for each).
-L.geoJson(data).addTo(map); 
-  
-  // {
-  // onEachFeature: function(feature, layer) {
-  // layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2><hr><h3>Airport name: "
-  // + feature.properties.name + "</h3>")}
-  // }).addTo(map)
+L.geoJson(data, {
+  style: myStyle,
+  onEachFeature: function(feature, layer) {
+  layer.bindPopup("<h2>Airline: " + feature.properties.airline + "</h2><hr><h3>Destination: "
+  + feature.properties.dst + "</h3>");
+  }
+  })
+  .addTo(map);
 });
   
